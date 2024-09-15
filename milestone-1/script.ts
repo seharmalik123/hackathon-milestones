f53754d6ee1d1a1a767cccb46c0b30c2772487ea
seharmalik123 <159246437+seharmalik123@users.noreply.github.com>
2024-09-15 16:03:53 +0500

    Add files via upload

---
+++ milestone-1/script.ts
@@ -0,0 +1,19 @@
+const button = document.getElementById("toggle-skills") as HTMLButtonElement;
+const skills = document.getElementById("skill-sec") as HTMLElement;
+
+button.addEventListener('click', () => {
+    if(skills.style.display === "none") {
+        skills.style.display = "block"
+    } else {
+        skills.style.display = "none"
+    }
+});
+
+
+
+
+
+
+
+
+
